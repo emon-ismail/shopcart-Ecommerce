a79c1a97ed46b6d7b102d0519d679b45f676eb63
Tonmoy-Roy <emonismail69@gmail.com>
2023-12-16 17:30:09 +0600

    Header section and animation done

---
+++ index.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Vanilla JavaScript for handling click events on navbar links
+  var navLinks = document.querySelectorAll('.navbar-nav a');
+
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      // Remove 'active-link' class from all links
+      navLinks.forEach(function (link) {
+        link.classList.remove('active-link');
+      });
+
+      // Add 'active-link' class to the clicked link
+      this.classList.add('active-link');
+    });
+  });
+
+  // Vanilla JavaScript for handling scroll events to reveal elements
+  window.addEventListener("scroll", reveal);
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+
+      if (revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+
+  var t1 = gsap.timeline();
+
+  // tl.from(".line span", 1.8, {
+  //   y: 100,
+  //   ease: "power4.out",
+  //   delay: 1,
+  //   skewY: 7,
+  //   stagger: {
+  //     amount: 0.3
+  //   }
+  // })
+
+  t1.from("nav img, nav h4, a, #drop",{
+    y:-100,
+    // duration:0.3,
+    // delay:0.09,
+    opacity:0,
+    // stagger:0.1
+  })
+
+  t1.from("#animate h1,p,#animContainer",{
+x:-200,
+opacity:0,
+stagger:0.1,
+  })
+
+
+
+
+  // Lottie Animation
+  var animation = lottie.loadAnimation({
+    container: document.getElementById("animContainer"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "/Animation - 1702544237754.json", // Lottie file path
+  });
+
+  // jQuery for Owl Carousel
+  $("#myCarousel").owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: [
+      "<i class='fas fa-chevron-left'></i>",
+      "<i class='fas fa-chevron-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+});
